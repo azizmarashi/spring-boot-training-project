e6c31b1c349a5791ff8958e6d80f22371f30be33
Ali <mohajer13sam@gmail.com>
2023-07-11 06:48:24 +0330

    add Serializable to UserDomain

--- src/main/java/com/example/springTraining/domain/UserDomain.java
+++ src/main/java/com/example/springTraining/domain/UserDomain.java
@@ -7,13 +7,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table
 @Entity
-public class UserDomain {
+public class UserDomain implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 2002549472325402634L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
